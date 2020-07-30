f4e4dee4ba04bf7a460c98028e1f2c23fe7606c2
Salifov <salifov17@gmail.com>
2020-07-30 10:45:16 +0300

    build

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
-    return ( 
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="navbar-brand" to="/">Vidly</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <NavLink className="nav-link nav-item" to="/movies">Movies <span className="sr-only">(current)</span></NavLink>
-                    <NavLink className="nav-link nav-item" to="/customers">Customers</NavLink>
-                    <NavLink className="nav-link nav-item" to="/rentals">Rentals</NavLink>
-                    <NavLink className="nav-link nav-item" to="/login">Login</NavLink>
-                    <NavLink className="nav-link nav-item" to="/register">Register</NavLink>
-                </div>
-            </div>
-        </nav>
-     );
-}
- 
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Movie Store
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <NavLink className="nav-link nav-item" to="/movies">
+            Movies <span className="sr-only">(current)</span>
+          </NavLink>
+          <NavLink className="nav-link nav-item" to="/customers">
+            Customers
+          </NavLink>
+          <NavLink className="nav-link nav-item" to="/rentals">
+            Rentals
+          </NavLink>
+          <NavLink className="nav-link nav-item" to="/login">
+            Login
+          </NavLink>
+          <NavLink className="nav-link nav-item" to="/register">
+            Register
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
